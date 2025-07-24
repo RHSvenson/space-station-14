d0b2f57e0ea80495dabac82b4a67de78d740fcf0
RHSvenson <konto@rhsvenson.dk>
2025-07-24 11:27:08 +0200

    Bones can contain organs, and have damage values and resistance. Todo: System for transferring damage to and from organs/limbs

---
+++ Content.Shared/_Starlight/Body/Organ/BoneComponent.cs
@@ -0,0 +1,41 @@
+using Content.Shared.Body.Systems;
+using Robust.Shared.Containers;
+using Robust.Shared.GameStates;
+using Content.Shared.Body.Components;
+using Content.Shared.Body.Part;
+
+namespace Content.Shared.Body.Bone;
+public sealed partial class BoneComponent : Component
+{
+    /// <summary>
+    /// The Uid of the body this bone is in
+    /// </summary>
+    [DataField, AutoNetworkedField]
+    public EntityUid? body;
+
+    /// <summary>
+    /// The part this bone belongs in, and which it functions in
+    /// </summary>
+    [DataField, AutoNetworkedField]
+    public BodyPartType intendedPart = BodyPartType.Other;
+
+    /// <summary>
+    /// Organs inside this bone.
+    /// </summary>
+    [DataField, AutoNetworkedField]
+    public Dictionary<string, OrganSlot> organs = new();
+
+    /// <summary>
+    /// Whether or not this bone is essential for moving the part it is in
+    /// </summary>
+    [DataField, AutoNetworkedField]
+    public bool kineticEssentiality = false;
+
+    /// <summary>
+    /// Whether or not this bone is essential for moving the child-parts of its host part.
+    /// Aka, if you break your hip, you can't use your legs either.
+    /// If you break your spine, nothing below it works either.
+    /// </summary>
+    [DataField, AutoNetworkedField]
+    public bool jointEssentiality = false;
+}
