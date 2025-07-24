4b3c0831b19c51866c19a7dd58f66aac952030b9
RHSvenson <konto@rhsvenson.dk>
2025-07-24 12:03:53 +0200

    bomboclat

--- Content.Shared/_Starlight/Body/Organ/BoneComponent.cs
+++ Content.Shared/_Starlight/Body/Organ/BoneComponent.cs
@@ -27,12 +27,6 @@ public sealed partial class BoneComponent : Component
     public Dictionary<string, OrganSlot> organs = new();
 
     /// <summary>
-    /// Whether or not this bone is essential for moving the part it is in
-    /// </summary>
-    [DataField, AutoNetworkedField]
-    public bool kineticEssentiality = false;
-
-    /// <summary>
     /// Whether or not this bone is essential for moving the child-parts of its host part.
     /// Aka, if you break your hip, you can't use your legs either.
     /// If you break your spine, nothing below it works either.
